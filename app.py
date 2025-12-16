a6c1e354fa23c29927dad507c49e2702d4e12cc8
Jan du Plessis <drjanduplessis@icloud.com>
2025-12-16 22:59:50 +0000

    init

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.header("The App Store")
+st.caption("Custom Streamlit apps to solve your usecase.")
+
+
+st.sidebar.header("Find Out more")
+st.sidebar.button("Button", type='primary')
